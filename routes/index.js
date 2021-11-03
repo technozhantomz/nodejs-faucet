5c08cd99275a9dc8ebdce91f09451d1bddab2803
technozhantomz <technoz.phantomz@commodity.llc>
2021-11-03 17:31:50 +0300

    ROUTING

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'BitShares Faucet API' });
+  res.render('index', { title: 'Commodity.LLC account creation API' });
 });
 
 module.exports = router;
